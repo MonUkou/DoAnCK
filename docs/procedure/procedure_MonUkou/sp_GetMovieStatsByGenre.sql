c03287df9bb62a66ba2398057b25f5011faeff22
MonUkou <monukou666@gmail.com>
2026-03-24 09:02:54 +0700

    Fix procedure to group by Genre_ID and Genre_Name

--- docs/procedure/procedure_MonUkou/sp_GetMovieStatsByGenre.sql
+++ docs/procedure/procedure_MonUkou/sp_GetMovieStatsByGenre.sql
@@ -1,11 +1,13 @@
-CREATE PROCEDURE sp_GetMovieStatsByGenre
-AS
+DELIMITER $$
+
+CREATE PROCEDURE sp_GetMovieStatsByGenre()
 BEGIN
     SELECT 
         g.Genre_Name AS Genre,
         COUNT(m.Movie_ID) AS MovieCount
     FROM tbl_genre g
     LEFT JOIN tbl_movie m ON g.Genre_ID = m.Genre_ID
-    GROUP BY g.Genre_Name;
-END
-GO
+    GROUP BY g.Genre_ID, g.Genre_Name;
+END$$
+
+DELIMITER ;
