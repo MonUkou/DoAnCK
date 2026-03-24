83d94083c87b9210b15abf52f7516f9dca7ad80e
MonUkou <monukou666@gmail.com>
2026-03-24 08:57:06 +0700

    Refactor sp_GetLatestMovies procedure syntax

--- docs/procedure/procedure_MonUkou/sp_GetLatestMovies.sql
+++ docs/procedure/procedure_MonUkou/sp_GetLatestMovies.sql
@@ -1,5 +1,6 @@
-CREATE PROCEDURE sp_GetLatestMovies
-AS
+DELIMITER $$
+
+CREATE PROCEDURE sp_GetLatestMovies()
 BEGIN
     SELECT 
         m.Movie_Title AS MovieTitle, 
@@ -8,4 +9,6 @@ BEGIN
     FROM tbl_movie m
     INNER JOIN tbl_genre g ON m.Genre_ID = g.Genre_ID
     ORDER BY m.Movie_ReleaseDate DESC;
-END
+END$$
+
+DELIMITER ;
